dddd78da7fd7e15a3eda0a30a78cba0d211e9946
dschere <dave.avantgarde@gmail.com>
2015-04-02 21:13:55 -0400

    update

--- setup.py
+++ setup.py
@@ -6,19 +6,17 @@ import os
 
 
 
-
-
 setup(
     name="dreadnought-js",   
     description = 'Dreadnought is a synthesis of cherrypy and JavaScript to create a full featured web server for JavaScript applications', 
-    version="0.1",
+    version="0.1.1",
     author="dschere",
     author_email="dave.avantgarde@gmail.com",
     url="https://github.com/dschere/dreadnought-js",
     download_url = "https://github.com/dschere/dreadnought -js/tarball/0.1",
     packages =["dnlib"] ,
     py_modules=['dnshell','dreadnought'],
-    package_data={'': ['LICENSE.txt','dn-env.sh','install.sh','favicon.ico','dn']},
+    package_data={'': ['CHANGES.txt','LICENSE.txt','dn-env.sh','install.sh','favicon.ico','dn']},
     keywords = ['webserver', 'JavaScript','cherrypy']
 )
 

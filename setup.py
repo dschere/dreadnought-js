bdaa6f5c0efaca05e7dcfd39463ec00ca1a07dfa
dschere <dave.avantgarde@gmail.com>
2015-04-08 09:36:06 -0400

    corrected missing dependancies.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email="dave.avantgarde@gmail.com",
     url="https://github.com/dschere/dreadnought-js",
     download_url = "https://github.com/dschere/dreadnought -js/tarball/0.1",
-    packages =["dnlib"] ,
+    packages =["dnlib","dnlib/PyInline"] ,
     py_modules=['dnshell','dreadnought'],
     package_data={'': ['CHANGES.txt','LICENSE.txt','dn-env.sh','install.sh','favicon.ico','dn']},
     keywords = ['webserver', 'JavaScript','cherrypy']
